929dc417dac70e0d751e690743ea83425f80cb10
Kalin Bowden <kalinbowden@gmail.com>
2019-06-21 21:00:59 -0500

    Class Stubs Added

---
+++ DiscordBotCore/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscordBotCore
+{
+    class User
+    {
+        public string ID { get; set; }
+        public string Username { get; set; }
+    }
+}
